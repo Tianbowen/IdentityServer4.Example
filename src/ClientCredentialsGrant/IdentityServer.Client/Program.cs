5ae3ff0e24d419ed74140a0869fc74714c872c65
arwen <1471758682@qq.com>
2020-06-04 14:17:35 +0800

    Add ClientCredentials

---
+++ src/ClientCredentialsGrant/IdentityServer.Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using static System.Console;
+
+namespace IdentityServer.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5001");
+            if (disco.IsError)
+            {
+                WriteLine("Disco Error:"+disco.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "clientCredentials",
+                ClientSecret = "ClientCredentialsSecret",
+                Scope = "api1"                
+            });
+
+            if (tokenResponse.IsError)
+            {
+                WriteLine($"Token Error:{tokenResponse.Error}");
+                return;
+            }
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var apiResponse = await apiClient.GetAsync("http://localhost:6001/identity");
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                WriteLine(apiResponse.StatusCode);
+            }
+            else
+            {
+                var content = await apiResponse.Content.ReadAsStringAsync();
+                WriteLine(JArray.Parse(content));
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ src/ClientCredentialsGrant/IdentityServer.ClientCredentialsGrant/Config.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer.ClientCredentialsGrant
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new List<ApiResource>
+            {
+                new ApiResource("api1","Test Api")
+            };
+        
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId="clientCredentials",
+                    //没有交互用户，使用clientid 或 secret 来进行身份验证
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    //用于身份验证的密钥
+                    ClientSecrets={ 
+                        new Secret("ClientCredentialsSecret".Sha256())
+                    },
+                    AllowedScopes={ "api1" }
+
+                }
+            };        
+    }
+}
